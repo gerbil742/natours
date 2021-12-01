43868848d5b6a10f01fc833311d4c88db69b15b0
Jibreel Campbell <jycampbell7@gmail.com>
2021-12-01 10:09:41 -0500

    created first 2 basic routs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 The Natours project is the main project behind the 42 hour Udemy course "Node.js, Express, MongoDB & More: The Complete Bootcamp 2022" by Jonas Schmedtmann.
-I will be following along with the instructor as he develops the project
+I will be following along with the instructor as he develops the project.
 I will be pushing all the code we have written in the project to Github in order to get more pratice using github and to showcase what i have learned throughout the course.
--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require('express');
 
 const app = express(); //the express() will add a bunch of methods to our app variable
 
-app.listen();
+app.get('/', (req, res) => {
+    //res.status(200).send('Hello form server side');
+    res.status(200).json({ message: 'Hello form server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+    res.send('Post to this endpoint');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
