151fa661ed41475097e046ea8eca857b034df9e7
Jibreel Campbell <jycampbell7@gmail.com>
2021-12-02 12:37:06 -0500

    created templates for the users route

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express(); //the express() will add a bunch of methods to our app variable
 
-// experss.json() is 'middleware'. basically just a fucntion that can modify the incoming request data
-// called middleware because it stands between the request and the response
+// MIDDLEWARES
+//
+
+// THese are 'middleware'. basically just a fucntion that can modify the incoming request data. called middleware because it stands between the request and the response
+app.use(morgan('dev')); // 3rd party middleware for getting more info about our requstes
 app.use(express.json());
 
-// We can create our own middlewares to add to teh middleware stack
-// if we include a 3rd argument, express knows that we are defining a middleware
+// We can create our own middlewares to add to teh middleware stack. if we include a 3rd argument, express knows that we are defining a middleware
 // Important to know that the Order matters of any middleware. If i were to include this func below my routes, then this would never get cdalled
 app.use((req, res, next) => {
   console.log('Hello form the middleware');
@@ -23,6 +26,9 @@ app.use((req, res, next) => {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// ROUTE HANDLERS
+//
 
 // use v1 in order to specify the api version. you can branch off to v2 and not break the v1 of the api
 // we call the callback the route handler
@@ -115,15 +121,56 @@ const deleteTour = (req, res) => {
   });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const getAllUsers = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'err',
+    message: 'This rount not defined',
+  });
+};
+
+const getUser = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'err',
+    message: 'This rount not defined',
+  });
+};
+
+const createUser = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'err',
+    message: 'This rount not defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'err',
+    message: 'This rount not defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  // 500 means internal server error
+  res.status(500).json({
+    status: 'err',
+    message: 'This rount not defined',
+  });
+};
+
+// ROUTES
+//
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+// SERVER
+//
 
 const port = 3000;
 app.listen(port, () => {
