cb44d137726889d4fa1f46dfd5ca7e00230892ab
Jibreel Campbell <jycampbell7@gmail.com>
2021-12-02 11:54:12 -0500

    implemented simple middleware

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
 const express = require('express');
-const { application } = require('express');
 
 const app = express(); //the express() will add a bunch of methods to our app variable
 
 // experss.json() is 'middleware'. basically just a fucntion that can modify the incoming request data
 // called middleware because it stands between the request and the response
 app.use(express.json());
+
+// We can create our own middlewares to add to teh middleware stack
+// if we include a 3rd argument, express knows that we are defining a middleware
+// Important to know that the Order matters of any middleware. If i were to include this func below my routes, then this would never get cdalled
+app.use((req, res, next) => {
+  console.log('Hello form the middleware');
+  // You need to call next() because otherwise the code wil just be stuck here
+  next();
+});
+
+app.use((req, res, next) => {
+  // adding a property to the req object that contains the current time of the request. Using middleware like this is a good way to add some additional into to our request
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -17,6 +31,7 @@ const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success', // can either have success(code 200, 201, etc), fail, or error
     results: tours.length,
+    requestedAt: req.requestTime,
     data: {
       tours: tours, // in es6 you can just write tours isf the key and the value are the same. You do need to make sure the property matches the endpoint name
     },
