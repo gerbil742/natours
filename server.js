3a624141242678132d404e5e1135e5a662b1f9d2
Jibreel Campbell <jycampbell7@gmail.com>
2021-12-07 13:54:49 -0500

    created connection strings and connected to the db

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' }); // This has to happen before we require the app file. otherwise, when we read the app file, the environment vars will be undefined.
 
 const app = require('./app');
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log('DB Connection successful');
+//   });
+
+const localDB = process.env.DATABASE_LOCAL;
+
+mongoose.connect(localDB).then(() => {
+  console.log('DB Connection successful');
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
