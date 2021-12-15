59599824b4c5dee44cf8deed1d525f2ccd51d137
Jibreel Campbell <jycampbell7@gmail.com>
2021-12-15 10:22:41 -0500

    added in basic global error handeling

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.use(express.static(`${__dirname}/public`)); // serving static files
 
 // creating our own middleware
 app.use((req, res, next) => {
-  console.log('Hello form the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); // adding a property to the req object
   next();
 });
@@ -28,5 +23,24 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Page not found catch all
+app.all('*', (req, res, next) => {
+  const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
